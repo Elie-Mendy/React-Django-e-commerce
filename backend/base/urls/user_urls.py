d50fdbe3e45c73de59221741d3b7cf67f1f65bd0
Elie MENDY <emendy@efficity.com>
2023-04-02 12:57:49 +0200

    backend endpoint update user profile

--- backend/base/urls/user_urls.py
+++ backend/base/urls/user_urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('register/', views.registerUser, name='register'),
     path('login/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('profile/', views.getUserProfile, name="users-profile"), 
+    path('profile/update/', views.updateUserProfile, name="users-profile-update"), 
 ]
